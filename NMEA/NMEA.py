270ff1e3f1dcfdae51c112a954ec29bc404819c0
Tim Hanewich <tahanewich@live.com>
2023-09-08 12:01:14 -0400

    Added info

--- NMEA/NMEA.py
+++ NMEA/NMEA.py
@@ -1,3 +1,16 @@
+"""
+A lightweight MicroPython implementation for parsing National Marine Electronics Association (NMEA) data.
+Author: Tim Hanewich - https://github.com/TimHanewich
+Version: 1.0
+Get updates to this code file here: https://github.com/TimHanewich/MicroPython-Collection/blob/master/NMEA/NMEA.py
+
+License: MIT License
+Copyright 2023 Tim Hanewich
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 import time
 import json
 
