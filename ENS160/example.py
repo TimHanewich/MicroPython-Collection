5999e9fcac86d3c02b491fa2e30c4abb5e392439
Tim Hanewich <tahanewich@live.com>
2024-06-12 16:51:28 -0400

    Updated example

--- ENS160/example.py
+++ ENS160/example.py
@@ -9,11 +9,7 @@ sensor = ENS160.ENS160(i2c)
 
 def loop():
     while True:
-        print("AQI: " + str(sensor.AQI))
-        print("TVOC: " + str(sensor.TVOC))
-        print("ECO2: " + str(sensor.ECO2))
-        print("Error: " + str(sensor.error))
-        print("New Data Available: " + str(sensor.new_data_available))
-        print("Signal rating: " + str(sensor.signal_rating))
-        print("------------------")
+        print("At ticks " + str(time.ticks_ms()))
+        print(str(sensor))
+        print("----------------")
         time.sleep(1)
