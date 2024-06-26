eda75a5da4e03354ef7daec5df1ad442098a6a64
Tim Hanewich <tahanewich@live.com>
2024-06-26 08:43:57 -0400

    Tweaked test 8

--- voltage-sensor/test8/main.py
+++ voltage-sensor/test8/main.py
@@ -3,7 +3,6 @@ import ssd1306
 import time
 import voltage
 import neopixel
-import color_toolkit
 
 # set up SSD1306
 i2c = machine.I2C(1, sda=machine.Pin(14), scl=machine.Pin(15))
@@ -18,30 +17,26 @@ pixels = neopixel.Neopixel(12, 0, 22, "GRB")
 # set up voltage sensor
 vs = voltage.VoltageSensor(26)
 
-def test() -> None:
+while True:
 
-    while True:
+    # handle pixels
+    if (time.ticks_ms() - last_switched_ticks_ms) > switch_every_ticks_ms:
+        if current_status == False:
+            pixels.fill((255, 255, 255))
+            current_status = True
+        else:
+            pixels.fill((0, 0, 0))
+            current_status = False
+        pixels.show() # show it
+        last_switched_ticks_ms = time.ticks_ms()
+    
+    # burst read
+    burst:int = vs._sample_analog(1.0, 40)
 
-        # handle pixels
-        if (time.ticks_ms() - last_switched_ticks_ms) > switch_every_ticks_ms:
-            if current_status == False:
-                pixels.fill((255, 255, 255))
-                current_status = True
-            else:
-                pixels.fill((0, 0, 0))
-                current_status = False
-            pixels.show() # show it
-            last_switched_ticks_ms = time.ticks_ms()
-        
-        # burst read
-        burst:int = vs._sample_analog(1.0, 40)
+    # display
+    oled.fill(0)
+    oled.text("0: " + str(burst), 0, 0)
+    oled.show()
 
-        # display
-        oled.fill(0)
-        oled.text("0: " + str(burst), 0, 0)
-        oled.show()
-
-        # sleep
-        time.sleep(0.25)
-
-test()
+    # sleep
+    time.sleep(0.25)
