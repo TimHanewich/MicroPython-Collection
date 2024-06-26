3937eb58fdf936799f9a6111baf5afe2a0d5aabf
Tim Hanewich <tahanewich@live.com>
2024-06-26 08:38:39 -0400

    On and off

--- voltage-sensor/test8/main.py
+++ voltage-sensor/test8/main.py
@@ -10,6 +10,9 @@ i2c = machine.I2C(1, sda=machine.Pin(14), scl=machine.Pin(15))
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 # set up neopixel
+last_switched_ticks_ms:int = 0
+switch_every_ticks_ms:int = 50000 # 5 seconds
+current_status:bool = False # off = False, on = True
 pixels = neopixel.Neopixel(12, 0, 22, "GRB")
 
 # set up voltage sensor
@@ -19,9 +22,16 @@ def test() -> None:
 
     while True:
 
-        # fill with random color
-        pixels.fill(color_toolkit.random_color())
-        pixels.show()
+        # handle pixels
+        if (time.ticks_ms() - last_switched_ticks_ms) > switch_every_ticks_ms:
+            if current_status == False:
+                pixels.fill((255, 255, 255))
+                current_status = True
+            else:
+                pixels.fill((0, 0, 0))
+                current_status = False
+            pixels.show() # show it
+            last_switched_ticks_ms = time.ticks_ms()
         
         # burst read
         burst:int = vs._sample_analog(1.0, 40)
