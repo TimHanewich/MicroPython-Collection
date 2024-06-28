24dd1569ca06ddf65eea71f37c14befedcb594a9
Tim Hanewich <tahanewich@live.com>
2024-06-28 13:02:33 -0400

    Updated colors to fill with some specific ones I need to check

--- NeopixelManager/tests/consumption_recording/main.py
+++ NeopixelManager/tests/consumption_recording/main.py
@@ -11,21 +11,18 @@ def fill_and_wait(color:tuple[int, int, int]) -> None:
 
 # inf loop
 while True:
-    fill_and_wait((50, 0, 0)) # Red x1
-    fill_and_wait((150, 0, 0)) # Red x2
-    fill_and_wait((255, 0, 0)) # Red x3
 
-    fill_and_wait((0, 50, 0)) # Green x1
-    fill_and_wait((0, 150, 0)) # Green x2
-    fill_and_wait((0, 255, 0)) # Green x3
+    #1's
+    fill_and_wait((1,1,1))
+    fill_and_wait((1,0,0))
+    fill_and_wait((0,1,0))
+    fill_and_wait((0,0,1))
 
-    fill_and_wait((0, 0, 50)) # Blue x1
-    fill_and_wait((0, 0, 150)) # Blue x2
-    fill_and_wait((0, 0, 255)) # Blue x3
-
-    fill_and_wait((50, 50, 50)) # White x1
-    fill_and_wait((150, 150, 150)) # White x2
-    fill_and_wait((255, 255, 255)) # White x3
+    #10's
+    fill_and_wait((10,10,10))
+    fill_and_wait((10,10,0))
+    fill_and_wait((0,10,10))
+    fill_and_wait((10,0,10))
 
     fill_and_wait((0, 0, 0)) # all off
     
