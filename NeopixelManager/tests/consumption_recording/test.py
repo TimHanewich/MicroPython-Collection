e563f14245fb84cafbb24af848c771127f103fa9
Tim Hanewich <tahanewich@live.com>
2024-06-28 11:30:41 -0400

    Removed two seemingly unneeded files

--- NeopixelManager/tests/consumption_recording/test.py
+++
@@ -1,9 +0,0 @@
-import neopixel
-import color_toolkit
-
-def test_gp(gp:int) -> None:
-    rc:tuple[int, int, int] = color_toolkit.random_color()
-    pixels = neopixel.Neopixel(12, 0, gp, "GRB")
-    pixels.fill(rc)
-    pixels.show()
-    print("Showed color " + str(rc) + " on GP " + str(gp))
