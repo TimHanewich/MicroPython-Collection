b01178554d94a4283993abbba8e0f400c88819ed
Tim Hanewich <tahanewich@live.com>
2024-06-28 11:34:27 -0400

    Updated test colors

--- NeopixelManager/tests/consumption_recording/main.py
+++ NeopixelManager/tests/consumption_recording/main.py
@@ -6,17 +6,26 @@ pixels = neopixel.Neopixel(12, 0, 22, "GRB")
 def fill_and_wait(color:tuple[int, int, int]) -> None:
     pixels.fill(color)
     pixels.show()
-    time.sleep(5)
+    time.sleep(10)
 
 # inf loop
 while True:
-    fill_and_wait((1, 1, 1)) # all white, min brightness
-    fill_and_wait((1, 0, 0)) # red
-    fill_and_wait((0, 1, 0)) # green
-    fill_and_wait((0, 0, 1)) # blue
-    fill_and_wait((10, 10, 10)) # all white, 10
-    fill_and_wait((10, 10, 0)) # 10
-    fill_and_wait((0, 10, 10)) # 10
-    fill_and_wait((10, 0, 10)) # 10
-    fill_and_wait((0, 0, 0)) # off
+    fill_and_wait((50, 0, 0)) # Red x1
+    fill_and_wait((150, 0, 0)) # Red x2
+    fill_and_wait((255, 0, 0)) # Red x3
+
+    fill_and_wait((0, 50, 0)) # Green x1
+    fill_and_wait((0, 150, 0)) # Green x2
+    fill_and_wait((0, 255, 0)) # Green x3
+
+    fill_and_wait((0, 0, 50)) # Blue x1
+    fill_and_wait((0, 0, 150)) # Blue x2
+    fill_and_wait((0, 0, 255)) # Blue x3
+
+    fill_and_wait((50, 50, 50)) # White x1
+    fill_and_wait((150, 150, 150)) # White x2
+    fill_and_wait((255, 255, 255)) # White x3
+
+    fill_and_wait((0, 0, 0)) # all off
+    
     
