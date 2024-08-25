915980305c024449f64ca0fb7f40f79e25dbdc28
Tim Hanewich <tahanewich@live.com>
2024-08-25 15:03:56 -0400

    Exampe

--- USB-Comms/example-pico/main.py
+++ USB-Comms/example-pico/main.py
@@ -3,40 +3,31 @@ import time
 import sys
 import select
 
-def example1():
+def example():
+
+    # initialize a "heap" - an array of bytes that we will constantly append to until we are ready to process what is there (i.e. a newline character/break character is in there)
+    heap:bytes = bytes()
+
+    # infinite loop to receive, process, and send
     while True:
         
-        # read all that are available, while there is some available
-        received:bytes = bytes() # collection heap
+        # read and collect all that are available, while there is some available
         while select.select([sys.stdin], [], [], 0)[0]: # there is at least 1 byte available to be read
-            data:bytes = sys.stdin.buffer.read(1) # collect that 1 byte
-            received = received + data # append it to the heap
+            data:bytes = sys.stdin.buffer.read(1) # collect that 1 byte (1 byte at a time)
+            heap = heap + data # append it to the heap
         
-        # if there was some data that we received (all collected at once), handle it
-        if len(received) > 0:
-            print("Received data: " + str(received))
-            
-        # handle what needs to be done (processing, reacting to inputs, etc.)
-        print("Hello at time " + str(time.ticks_ms()) + "!")
-        
-        # small wait period
-        time.sleep(3)
+        # handle what was received if there is a sequence split in it
+        # the sequence split just means the client (PC) has written what it intended to write. Usually a newline (\n or \r\n) character, but you can set it to whatever you want.
+        split_sequence:bytes = "\r".encode()
+        while split_sequence in heap: # if the heap has the split sequence in it
 
-def example2():
-    while True:
-        
-        # If there is data available, what for it all to finish (until a newline character is received)
-        if select.select([sys.stdin], [], [], 0)[0]: # there is at least 1 byte available to be read, so we are being sent something!
-            
-            # read the data line!
-            # this will read the entirety of the bytes available to read, but will wait until the newline character is at the end to continue. 
-            # This IS a blocking function, so the program will stop and sit here until the newline is received. 
-            # In other words, it is sort of like "waiting for the PC (client) to finish sending what it is in the middle of sending".
-            data:bytes = sys.stdin.buffer.readline() 
+            # extract the first chunk, split from the rest by a split sequence
+            split_position:int = heap.find(split_sequence) # find where the split sequence is
+            chunk:bytes = heap[0:split_position] # extract the first chunk, but not the split sequence
+            heap = heap[split_position + 1:] # set the heap to the remainder, stripping out both the first chunk and the split sequence
 
-            # handle the data however necessary
-            print("Just received this data: " + str(data))
-            
+            # handle this chunk
+            print("Chunk: " + str(chunk))
             
         # handle what needs to be done (processing, reacting to inputs, etc.)
         print("Hello at time " + str(time.ticks_ms()) + "!")
@@ -44,4 +35,4 @@ def example2():
         # small wait period
         time.sleep(0.5)
 
-example2()
+example()
