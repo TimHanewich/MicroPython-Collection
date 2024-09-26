a4339fb1e97f8e1a53049d035c87cfea81699ec0
Tim Hanewich <tahanewich@live.com>
2024-09-26 09:47:59 -0400

    Removed test

--- REYAX-RYLR998/reyax.py
+++ REYAX-RYLR998/reyax.py
@@ -301,13 +301,3 @@ class RYLR998:
             raise Exception("Response from RYLY998 for command " + str(command) + " was not received after waiting " + str(response_timeout_ms) + " ms!")
 
         return response
-    
-
-
-import machine
-u = machine.UART(0, baudrate=115200, tx=machine.Pin(16), rx=machine.Pin(17))
-lora = RYLR998(u)
-
-for _ in range(10):
-    print(lora._command_response("AT\r\n"))
-    print(lora._rxbuf)
