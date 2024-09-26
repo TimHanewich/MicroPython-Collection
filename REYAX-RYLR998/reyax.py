0e6121c8acd602135149e8f9fa824b515fe48ce2
Tim Hanewich <tahanewich@live.com>
2024-09-26 09:41:47 -0400

    Removed old _command_response() and replaced with new (v2)

--- REYAX-RYLR998/reyax.py
+++ REYAX-RYLR998/reyax.py
@@ -276,44 +276,6 @@ class RYLR998:
         # but rather a message that pops up when a message is received (not directly a result of a command sent to the RYLR998 via UART)
         # Because of this rule, any +RCV will just be ignored and be stored in the buffer.
 
-        # collect any bytes still left over in UART Rx and make note of the length of the internal buffer before the command is sent out and response for it is received
-        self._colrx()
-        len_before:int = len(self._rxbuf)
-
-        # send command
-        self._uart.write(command)
-
-        # wait max time for bytes to be available
-        started_waiting_at_ticks_ms:int = time.ticks_ms()
-        while (time.ticks_ms() - started_waiting_at_ticks_ms) < response_timeout_ms and self._uart.any() == 0:
-            time.sleep_ms(1)
-
-        # collect any new bytes in UART Rx
-        self._colrx()
-
-        # count the number of new bytes that were just added!
-        new_bytes_count:int = len(self._rxbuf) - len_before
-
-        # if there are not any new bytes in the internal buf, it failed!
-        if new_bytes_count == 0:
-            raise Exception("Response from RYLY998 for command " + str(command) + " was not received after waiting " + str(response_timeout_ms) + " ms!")
-        
-        # get the ones we just received
-        response:bytes = self._rxbuf[-new_bytes_count:]
-
-        # trim the internal buffer now that we just "plucked" the response out of it
-        self._rxbuf = self._rxbuf[0:-new_bytes_count]
-
-        return response
-    
-    def _command_response_v2(self, command:bytes, response_timeout_ms:int = 500)-> bytes:
-        """Sends a byte sequence (AT command) to the RYLR988 module, and collects the response while still preserving any pre-existing bytes in the internal Rx buffer."""
-
-        # the _command_response() function should never return a "+RCV" response. +RCV is the message that shows up when a message was received.
-        # the reason it should never return an +RCV is because +RCV is NEVER the RESPONSE to a command,
-        # but rather a message that pops up when a message is received (not directly a result of a command sent to the RYLR998 via UART)
-        # Because of this rule, any +RCV will just be ignored and be stored in the buffer.
-
         # collect any bytes still left over in UART Rx 
         # we do this just in case there were previously un-handled response bytes (i.e. +RCV)
         new_bytes:bytes = self._uart.read()
