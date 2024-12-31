7e045afa7f014c83609777d6e39293d27e36bf31
Tim Hanewich <tahanewich@live.com>
2024-12-31 10:05:49 -0500

    Added TOF

---
+++ VL53L0X/src/main.py
@@ -0,0 +1,13 @@
+import time
+import vl53l0x
+import machine
+
+i2c = machine.I2C(1, sda=machine.Pin(14), scl=machine.Pin(15))
+print(i2c.scan())
+
+tof = vl53l0x.VL53L0X(i2c)
+
+while True:
+    distance:int = tof.ping() - 50 # subtract 50 to account for error (it always estimates 50 over!)
+    print("Distance: " + str(distance) + " mm")
+    time.sleep(0.25)
