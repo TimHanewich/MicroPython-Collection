de3d9cf6de526b78034302cb157f98130fec80b7
Tim Hanewich <tahanewich@live.com>
2026-06-06 22:16:26 -0400

    Working

--- INMP411/example.py
+++ INMP411/example.py
@@ -9,23 +9,34 @@ audio_in = I2S(0, sck=Pin(16), ws=Pin(17), sd=Pin(18), mode=I2S.RX, bits=32, for
 # Set the length to what you calculate is the length needed given the duration you want and sample rate being used
 read_buffer = bytearray(8_000)
 
-# read
-print("Reading now.")
-t1 = time.ticks_ms()
-bytes_read = audio_in.readinto(read_buffer)
-t2 = time.ticks_ms()
-print(str(len(read_buffer)) + " bytes read in " + str(t2 - t1) + " ms: " + str(read_buffer))
+for i in range(0, 20):
+
+    # read
+    #print("Reading sample # " + str(i) + "... ")
+    t1 = time.ticks_ms()
+    bytes_read = audio_in.readinto(read_buffer)
+    t2 = time.ticks_ms()
+    #print(str(len(read_buffer)) + " bytes read in " + str(t2 - t1) + " ms: " + str(read_buffer))
+
+    # How many actual samples did we just get (each sample is 4 bytes)
+    num_samples:int = bytes_read // 4
+
+    # calc loudness across this sample
+    all_squares:int = 0
+    for s in range(num_samples):
+        # convert to int
+        byte1_index:int = s * 4      # the index of the first byte of the 4-byte integer
+        rawint:int = int.from_bytes(read_buffer[byte1_index:byte1_index+4], "little")
+        if rawint >= 0x80000000:
+            rawint = rawint - 0x100000000
+        all_squares = all_squares + rawint**2
+    all_squares_avg = all_squares / num_samples
+    vol:float = math.sqrt(all_squares_avg)
+    print("Volume: " + str(vol))
+    
 
 # deinit
 print()
 print("De-init...")
 audio_in.deinit()
 print("Done. Bye!")
-
-# calc
-all_squares:int = 0
-for b in read_buffer:
-    all_squares = all_squares + b**2
-all_squares_avg = all_squares / len(read_buffer)
-vol:float = math.sqrt(all_squares_avg)
-print("Volume: " + str(vol))
